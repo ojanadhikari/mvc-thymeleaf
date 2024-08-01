b8bbf07435c0c8bbd13aae49d997550cee7ff498
ojan <ojan.adhikari@wealthdoor.com>
2024-08-01 08:26:31 +0545

    eureka and cloud

--- src/main/java/com/bo/cabmvc/CabMvcApplication.java
+++ src/main/java/com/bo/cabmvc/CabMvcApplication.java
@@ -2,10 +2,12 @@ package com.bo.cabmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CabMvcApplication {
 
     public static void main(String[] args) {
